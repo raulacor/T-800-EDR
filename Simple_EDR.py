03114d2f92abf25d7e1d81de2e6c99fa0dad02d2
Raul Azzi Corsi <raul.acorsi@gmail.com>
2025-03-26 10:52:23 -0300

    Simple_EDR_V.0.1.py

--- Simple_EDR.py
+++ Simple_EDR.py
@@ -3,9 +3,14 @@ import time
 import logging
 
 logging.basicConfig(filename="edr_log.txt", level=logging.INFO, format="%(asctime)s - %(message)s")
-SUSPICIUS_PROCESSES = {"mimikatz.exe", "darkcometer.exe", "meterpreter.exe"}
 
-def get_running_processes ():
+#Define alguns processos/executaveis suspeitos
+SUSPICIOUS_PROCESSES = ["cmd.exe", "powershell.exe ", "bash", "sh", "python.exe" "mimikatz.exe", "darkcometer.exe", "meterpreter.exe"]
+SUSPICIOUS_EXTENSIONS = [".bat", ".sh", ".py"]
+
+
+def get_running_processes():
+    """Retorna uma lista dos processos que estão rodando atualmente no computador"""
     return {p.info['name'] for p in psutil.processes_iter(attrs=['name'])}
 
 def main():
@@ -16,10 +21,24 @@ while True:
     time.sleep(3)
     new_processes = get_running_processes
 
+
     started_processes = get_running_processes - old_processes()
+    """Detecta processos iniciados recentemente"""
     for process in started_processes: 
         logging.info(f"New process detected: {process}")
         print(f"New process detected: {process}")
 
-        if process.lower() in SUSPICIOUS_PROCESSES:
-            logging.warning(f"Suspicious process detected: {process}")
+        if process.lower() in SUSPICIOUS_PROCESSES or SUSPICIOUS_EXTENSIONS:
+            logging.warning(f"[ALERTA] Processo suspeito: {process}")
+            print(f"[ALERTA] Processo suspeito detectado: {process}")
+
+            for proc in psutil.process_iter(attrs=["name", "pid"]):
+                if proc.info["name"] == process:
+                    psutil.Process(proc.info['pid']).terminate()
+                    logging.info(f"Processo terminado: {process}")
+                    print(f"Processo encerrado: {process}")
+
+    old_processes = new_processes
+
+if __name__ == "__main__":
+    main()
