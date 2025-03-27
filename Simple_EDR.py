b81519b4bc6395303da4157e275b576155e73c27
Raul Azzi Corsi <raul.acorsi@gmail.com>
2025-03-27 11:09:30 -0300

    Update Simple_EDR.py
    03.27.25
    
    Recuo;  "main" para "process_monitoring"; formatação

--- Simple_EDR.py
+++ Simple_EDR.py
@@ -5,40 +5,45 @@ import logging
 logging.basicConfig(filename="edr_log.txt", level=logging.INFO, format="%(asctime)s - %(message)s")
 
 #Define alguns processos/executaveis suspeitos
-SUSPICIOUS_PROCESSES = ["cmd.exe", "powershell.exe ", "bash", "sh", "python.exe" "mimikatz.exe", "darkcometer.exe", "meterpreter.exe"]
-SUSPICIOUS_EXTENSIONS = [".bat", ".sh", ".py", ".vbs"]
+SUSPICIOUS_PROCESSES = {"cmd.exe", "powershell.exe ", "bash", "sh", 
+                        "python.exe" "mimikatz.exe", "darkcometer.exe", 
+                        "meterpreter.exe"
+                        }
+
+SUSPICIOUS_EXTENSIONS = {".bat", ".sh", ".py"}
 
 
 def get_running_processes():
     """Retorna uma lista dos processos que estão rodando atualmente no computador"""
     return {p.info['name'] for p in psutil.processes_iter(attrs=['name'])}
 
-def main():
-    print("[EDR iniciado] Monitorando processos. . . ")
+def process_monitoring():
+    print("EDR inciado. Monitorando processos. . . ")
     old_processes = get_running_processes()
-    
-while True:
-    time.sleep(3)
-    new_processes = get_running_processes
+    while True:
+        time.sleep(3) 
+        """Determina o intervalo de verificações"""
+        new_processes = get_running_processes
 
 
-    started_processes = get_running_processes - old_processes()
-    """Detecta processos iniciados recentemente"""
-    for process in started_processes: 
-        logging.info(f"Novo processo detectado: {process}")
-        print(f"Nove processo detectado: {process}")
+        started_processes = get_running_processes - old_processes()
+        """Detecta processos iniciados recentemente"""
+        for process in started_processes: 
+            logging.info(f"New process detected: {process}")
+            print(f"New process detected: {process}")
 
-        if process.lower() in SUSPICIOUS_PROCESSES or SUSPICIOUS_EXTENSIONS:
-            logging.warning(f"[ALERTA] Processo suspeito: {process}")
-            print(f"[ALERTA] Processo suspeito detectado: {process}")
+            if process.lower() in SUSPICIOUS_PROCESSES or SUSPICIOUS_EXTENSIONS:
+                logging.warning(f"[ALERTA] Processo suspeito: {process}")
+                print(f"[ALERTA] Processo suspeito detectado: {process}")
 
-            for proc in psutil.process_iter(attrs=["name", "pid"]):
-                if proc.info["name"] == process:
-                    psutil.Process(proc.info['pid']).terminate()
-                    logging.info(f"Processo encerrado: {process}")
-                    print(f"Processo encerrado: {process}")
+                for proc in psutil.process_iter(attrs=["name", "pid"]):
+                    if proc.info["name"] == process:
+                        psutil.Process(proc.info['pid']).terminate()
+                        logging.info(f"Processo encerrado: {process}")
+                        print(f"Processo encerrado: {process}")
 
-    old_processes = new_processes
+                        old_processes = new_processes
 
 if __name__ == "__main__":
-    main()
+    print("[INFO] Monitoramento inicializado. . .")
+    process_monitoring()
